4f79553b730bae8c83c369c1dbe82791fa69e15d
Willian Herrera <wherrera4@gmail.com>
2025-07-16 00:12:46 -0500

    Se agrega las validaciones y eventos en input-range

--- components/input-range.js
+++ components/input-range.js
@@ -1,15 +1,40 @@
 class InputRange extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
+    // Se crea el Shadow DOM
+    this.attachShadow({ mode: 'open' }); 
   }
 
   connectedCallback() {
+    // Se define el contenido del Shadow DOM
     this.shadowRoot.innerHTML = `
       <label>Inicio: <input type="number" id="inicio"></label>
       <label>Fin: <input type="number" id="fin"></label>
       <button id="enviar">Enviar</button>
     `;
+
+    // Se agrega el evento al botón
+    this.shadowRoot.querySelector('#enviar').addEventListener('click', () => {
+      const inicio = parseInt(this.shadowRoot.querySelector('#inicio').value);
+      const fin = parseInt(this.shadowRoot.querySelector('#fin').value);
+        
+      // Validación de los valores ingresados
+      if (isNaN(inicio) || isNaN(fin)) {
+        alert('Por favor, ingrese números válidos.');
+        return;
+      }
+
+      if (inicio > fin) {
+        alert('El valor de inicio debe ser menor o igual al valor del campo fin.');
+        return;
+      }
+
+      this.dispatchEvent(new CustomEvent('rango-seleccionado', {
+        detail: { inicio, fin },
+        bubbles: true,
+        composed: true
+      }));
+    });
   }
 }
 
